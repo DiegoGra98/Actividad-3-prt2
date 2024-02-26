be1f49f9db03ed00ff1e69b7ac449b82beb3a2fd
dalfr <dvallei@miumg.edu.gt>
2024-02-25 20:30:18 -0600

    clase cliente commit

---
+++ ActividadPrt2/src/actividadprt2/cliente.java
@@ -0,0 +1,15 @@
+package actividadprt2;
+
+public class cliente {
+
+    private String nit = "";
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+}
